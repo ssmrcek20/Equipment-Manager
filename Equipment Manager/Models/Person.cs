de1b80a7bda5688e60009a7786bc5517bddb4016
Stanko Smrček <ssmrcek20@foi.hr>
2022-05-24 20:27:47 +0200

    Stvaranje svih osoba

---
+++ Equipment Manager/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipment_Manager.Models
+{
+    public abstract class Person
+    {
+        public int ID { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+    }
+}
