8293c09f9d166c5b27ab772188c0a593cbbc726e
Stanko Smrček <ssmrcek20@foi.hr>
2022-05-25 10:49:41 +0200

    Posudba i posudbaRepostiory code

---
+++ Equipment Manager/Models/Posudba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipment_Manager.Models
+{
+    public class Posudba
+    {
+        public int ID { get; set; }
+        public int IDCipZaposlenika { get; set; }
+        public int IDFoiZaposlenika { get; set; }
+        public string DatumPosudbe { get; set; }
+        public string RazlogPosudbe { get; set; }
+        public string DatumVracanja { get; set; }
+        public int IDOpreme { get; set; }
+    }
+}
--- Equipment Manager/Repositories/PosudbaRepository.cs
+++ Equipment Manager/Repositories/PosudbaRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DBLayer;
+using Equipment_Manager.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,64 @@ namespace Equipment_Manager.Repositories
 {
     public class PosudbaRepository
     {
-        
+        public static Posudba GetPosudba(int id)
+        {
+            Posudba posudba = null;
+
+            string sql = $"SELECT * FROM Posudba WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                posudba = CreateObject(reader);
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return posudba;
+        }
+
+        public static List<Posudba> GetPosudbe()
+        {
+            var posudbe = new List<Posudba>();
+
+            string sql = "SELECT * FROM Posudba";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Posudba posudba = CreateObject(reader);
+                posudbe.Add(posudba);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return posudbe;
+        }
+
+        private static Posudba CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["ID"].ToString());
+            int idCipZaposlenika = int.Parse(reader["IDCip"].ToString());
+            int idFoiZaposlenika = int.Parse(reader["IDFoi"].ToString());
+            string datumPosudbe = reader["DatumPosudbe"].ToString();
+            string razlogPosudbe = reader["RazlogPosudbe"].ToString();
+            string datumVracanja = reader["DatumVracanja"].ToString();
+            int idOpreme = int.Parse(reader["IDOpreme"].ToString());
+
+            var posudba = new Posudba
+            {
+                ID = id,
+                IDCipZaposlenika =idCipZaposlenika,
+                IDFoiZaposlenika=idFoiZaposlenika,
+                DatumPosudbe = datumPosudbe,
+                DatumVracanja = datumVracanja,
+                IDOpreme = idOpreme
+            };
+
+            return posudba;
+        }
     }
 }
